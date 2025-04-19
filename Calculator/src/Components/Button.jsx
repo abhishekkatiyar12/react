811c1cc99c50e7aaae793c423fc24f846e1f814c
abhishekkatiyar12 <abhishek.katiyar.983932@gmail.com>
2025-04-19 22:15:13 +0530

    changes done

--- Calculator/src/Components/Button.jsx
+++ Calculator/src/Components/Button.jsx
@@ -4,13 +4,6 @@ function Button({handleclick}){
 
     let buttons=['C','1','2','3','4','5','6','7','8','9','0','*','/','-','+','='];
 
-//    function handleclick(event){
-//          console.log(event.target.innerText)
-//    }
-
-
-
-
     return (
         <>
         <div className={Styles["button-container"]}>
