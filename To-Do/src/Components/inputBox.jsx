ebe3148ca1360dc809f3c3c14df5d530f3d352b3
abhishekkatiyar12 <abhishek.katiyar.983932@gmail.com>
2025-04-19 23:30:07 +0530

    used use Ref

--- To-Do/src/Components/inputBox.jsx
+++ To-Do/src/Components/inputBox.jsx
@@ -1,19 +1,29 @@
 
 import Styles from './inputBox.module.css';
 
-function InputBox({handleclick,addItem,input}) {
-    
+function InputBox({addItem, input, date }) {
+
 
     return (
         <>
             <div className={Styles.input}>
-                <input 
-                    type="text"  
-                    placeholder="Enter the task you want to Add" 
-                    className={Styles.inputStyle} 
-                    onChange={handleclick}
-                    value={input}
-                    />
+                <input
+                    type="text"
+                    placeholder="Enter the task you want to Add"
+                    className={Styles.inputStyle}
+                    ref={input}
+                    // onChange={()=>{}}
+                    // value={input}
+                />
+                <input
+                    type="date"
+                    className={Styles.inputStyle}
+                    // value={date}
+                    ref={date}
+                    // onChange={()=>{}}
+                   
+                    
+                />
                 <button className={Styles.inputStyle} onClick={addItem}>Add</button>
             </div>
         </>
